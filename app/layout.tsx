b378202b5972d92b454e5812a4ad7f3ae9977137
MD BILLAL <hzbillal303@gmail.com>
2025-01-29 03:45:08 +0600

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anik deb's protfolio",
+  title: "Billal deb's protfolio",
   description: "Attracted catching portfolio",
 };
 
